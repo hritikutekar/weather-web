457c57a57fdc39bc41826144d237fd29af2aa919
hritikutekar <hritikdrocks@gmail.com>
2021-06-17 09:20:49 +0530

    update api key for opencagedata

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const OPEN_WEATHER_API_KEY = "36154bd500e9a654cfadd0a397e9f137";
 export const OPEN_WEATHER_URL = "https://api.openweathermap.org/data/2.5";
 
-export const OPEN_CAGEDATE_API_KEY = "6f8d3a1f8cd349d38bfe834bb5623f00";
+export const OPEN_CAGEDATE_API_KEY = "7613ccecc9214805b36c93cc8f80de33";
 export const OPEN_CAGEDATE_URL = "https://api.opencagedata.com/geocode/v1/json";
 
 export const UNSPLASH_API = "https://api.unsplash.com";
